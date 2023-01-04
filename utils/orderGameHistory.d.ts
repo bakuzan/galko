75f186a1319254233db1acfaff9b638b92d2a887
bakuzan <imannoyed4days@hotmail.co.uk>
2023-01-04 14:54:11 +0000

    Updates

--- utils/orderGameHistory.d.ts
+++ utils/orderGameHistory.d.ts
@@ -1,4 +1,4 @@
 import { GameResultView } from '@/interfaces/GameResult';
 export declare const resultFields: readonly ["timeElapsed", "datetime", "pairs", "longestStreak", "matchAttemptsRatio"];
 export declare type ResultField = typeof resultFields[number];
-export default function orderGameHistory(history: GameResultView[], key: ResultField, order: number): any[];
+export default function orderGameHistory(history: GameResultView[], key: ResultField, order: number): GameResultView[];
