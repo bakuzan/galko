0296cf0d2cffeda7dc18c50ec434d2f8468a71e8
bakuzan <imannoyed4days@hotmail.co.uk>
2023-01-20 11:08:03 +0000

    Updates

---
+++ utils/cardGrid.d.ts
@@ -0,0 +1,3 @@
+export declare function getElementWidth<T extends Pick<HTMLElement, 'getBoundingClientRect'>>(element: T): number;
+export declare function getCardWidth(): 103 | 64 | 83;
+export declare function getHighestEvenColumns(cards: number, maxColumns: number): number;
